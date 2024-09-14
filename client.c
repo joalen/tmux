ca5bb45742bd2282086f4fbbbda173b6bfc5be5a
Alen Jo <45494246+joalen@users.noreply.github.com>
2024-09-14 16:39:16 -0500

    Update client.c
    segfault happened because i commented out not very needy functions. This time, I commented out more stuff that is called from empty uninitialized vars.

--- client.c
+++ client.c
@@ -335,9 +335,9 @@ client_main(struct event_base *base, int argc, char **argv, uint64_t flags,
 	/* Free stuff that is not used in the client. */
 	if (ptm_fd != -1)
 		close(ptm_fd);
-	options_free(global_options);
+	//options_free(global_options);
 	options_free(global_s_options);
-	options_free(global_w_options);
+	//options_free(global_w_options);
 	environ_free(global_environ);
 
 	/* Set up control mode. */
